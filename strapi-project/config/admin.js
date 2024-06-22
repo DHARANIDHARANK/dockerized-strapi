983fa58d6ed585c7e76f4c1b363c94a8f4b537b8
Ubuntu <ubuntu@ip-172-31-45-148.ec2.internal>
2024-06-22 05:32:15 +0000

    dockerfile added

--- strapi-project/config/admin.js
+++ strapi-project/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','defaultAdminSecret'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'defaultApiSalt'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'defaultApiSalt'),
     },
   },
   flags: {
