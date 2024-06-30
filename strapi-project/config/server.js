15f8f09e0175b2172e95c1b3a2c0c85b6e85ba27
Dharanidharan K <75402352+DHARANIDHARANK@users.noreply.github.com>
2024-06-30 18:06:29 +0530

    Update server.js
    Signed-off-by: Dharanidharan K <75402352+DHARANIDHARANK@users.noreply.github.com>

--- strapi-project/config/server.js
+++ strapi-project/config/server.js
@@ -2,9 +2,15 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS',['myKeyA', 'myKeyB']),
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  url: env('PUBLIC_ADMIN_URL', '/admin'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
+    },
   },
 });
